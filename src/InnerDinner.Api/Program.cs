8f1b64aee2232ccab333e2f26feb3111dd841f67
BISHORUP BANJARA <bishorup.banjara@outlook.com>
2022-08-07 01:18:18 -0500

    Added global error handling

--- src/InnerDinner.Api/Program.cs
+++ src/InnerDinner.Api/Program.cs
@@ -1,6 +1,8 @@
+using InnerDinner.Api.Errors;
 using InnerDinner.Application;
 using InnerDinner.Application.Services.Authentication;
 using InnerDinner.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
@@ -9,10 +11,12 @@ var builder = WebApplication.CreateBuilder(args);
         .AddInfrastructure(builder.Configuration);
 
     builder.Services.AddControllers();
+    builder.Services.AddSingleton<ProblemDetailsFactory, InnerDinnerProblemDetailsFactory>();
 }
 
 var app = builder.Build();
 {
+    app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.MapControllers();
 }
